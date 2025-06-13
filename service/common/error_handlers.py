4b7ca04698943f48ce97139c5befdd48bf157bfd
waleed <waleed.walli364@gmail.com>
2025-06-13 08:40:47 -0400

    made some changes

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -44,11 +44,14 @@ def not_found(error):
     """Handles resources not found with 404_NOT_FOUND"""
     message = str(error)
     app.logger.warning(message)
-    return (
-        jsonify(status=status.HTTP_404_NOT_FOUND,
-        error="Not Found", message=message),
-               status.HTTP_404_NOT_FOUND,
-            )
+   return (
+    jsonify(
+        status=status.HTTP_404_NOT_FOUND,
+        error="Not Found",
+        message=message
+    ),
+    status.HTTP_404_NOT_FOUND,
+    )
 
 
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
--- service/routes.py
+++ service/routes.py
@@ -43,7 +43,7 @@ def list_counters():
  counters = [
               dict(name=count[0], 
               counter=count[1]) for count in COUNTER.items()
-            ]
+    ]
 
     return jsonify(counters)
 
