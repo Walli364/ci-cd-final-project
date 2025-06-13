e9cede19446e1bb1c2a8d666de43d4826dfbea94
waleed <waleed.walli364@gmail.com>
2025-06-13 08:21:07 -0400

    made some changes

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -32,7 +32,7 @@ def bad_request(error):
     app.logger.warning(message)
     return (
         jsonify(
-          status=status.HTTP_400_BAD_REQUEST, 
+          status=status.HTTP_400_BAD_REQUEST,
           error="Bad Request", message=message
         ),
         status.HTTP_400_BAD_REQUEST,
@@ -45,10 +45,9 @@ def not_found(error):
     message = str(error)
     app.logger.warning(message)
     return (
-        jsonify(status=status.HTTP_404_NOT_FOUND, 
+        jsonify(status=status.HTTP_404_NOT_FOUND,
         error="Not Found", message=message),
-        status.HTTP_404_NOT_FOUND,
-    )
+               status.HTTP_404_NOT_FOUND,)
 
 
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
